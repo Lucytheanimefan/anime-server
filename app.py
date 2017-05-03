4e36b20b681fe3699a5bcc14b7e2a55b256c0f7b
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-05-03 18:21:14 -0400

    write to database

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 import os
-from flask import Flask, render_template,send_from_directory, jsonify
+from flask import Flask, render_template,send_from_directory, jsonify, request
 import requests
 import server
 
 app = Flask(__name__)
+database = server.get_db()
 
+@app.route("/")
+def home():
+	return render_template("index.html")
 
 @app.route("/addNews", methods=["POST"])
 def add_news():
+	print "ADD NEWS"
+	news = request.get_json()
+	print news
+	database.articles.insert_one(request.get_json())
+	return "DONE"
+
+
+@app.route("/getNews", methods=["GET"])
+def get_news():
 	news = request.get_json()
 	print news
 
