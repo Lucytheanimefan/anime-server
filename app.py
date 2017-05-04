25e978f992e67bd69fa284794ba5cbfb5573247c
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-05-03 22:09:19 -0400

    GET request working

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template,send_from_directory, jsonify, request
 import requests
 import server
 import JSONEncoder
+import time
 
 app = Flask(__name__)
 database = server.get_db()
@@ -13,21 +14,23 @@ def home():
 
 @app.route("/addNews", methods=["POST"])
 def add_news():
-	print "ADD NEWS"
-	news = request.form
-	database.articles.insert_one(dict(news))
-	return "DONE"
+	news = dict(request.form)
+	news["date"]=time.strftime("%m/%d/%Y") 
+	database.articles.insert_one(news)
+	#database.titles.insert_one({"title":news["title"],"date":news["date"]})
+	return home()
 
 
 @app.route("/getNews", methods=["GET"])
 def get_news():
 	news = database.articles.find({})
+	articles = []
 	for doc in news:
-		print doc
-	#print news
-	articles = [JSONEncoder().encode(data) for data in news]
-	print articles
+		del doc["_id"]
+		articles.append(doc)
+
 	return jsonify(articles)
+	#return jsonify(articles)
 
 
 
