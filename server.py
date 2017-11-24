f177028376ef75bcf5efa434c07f5e127ce95263
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-11-23 20:31:45 -0500

    deployment friendly

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-from config import *
+#from config import *
 #from boto.s3.connection import S3Connection
 
 from os import environ
@@ -8,8 +8,8 @@ from os import environ
 #s3 = S3Connection(os.environ['PROD_MONGODB'], os.environ['CLIENT_MONGODB'])
 
 def get_db():
-    client = MongoClient(MONGODB_URI)
+    client = MongoClient(environ.get('MONGODB_URI'))
     '''environ.get('MONGODB_URI')'''
-    db = client[CLIENT_MONGODB] 
+    db = client[environ.get('CLIENT_MONGODB')] 
     '''environ.get('CLIENT_MONGODB')'''
     return db
