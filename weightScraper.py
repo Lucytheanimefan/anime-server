e0bd33b0455f95edd2912461e655d44df732c172
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-02-16 06:04:57 -0500

    Relocated anime weight scraper to anime server

---
+++ weightScraper.py
@@ -0,0 +1,76 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import json
+import urllib
+
+
+characters = ['Shiki Ryougi', 'Haruhi Fujioka', 'Nanami Momozono', 'Saber', 'Rei Kiriyama', 'Mikasa Ackerman', 'Hori Kyouko', 'Miyamura_Izumi', 'Yuuki Asuna', 'Yukino_Yukinoshita', 'Misaki_Ayuzawa', 'Touka_Kirishima', 'Akatsuki no Yona Yona', 'Inori_Yuzuriha', 'Misa Amane', 'Historia Reiss', 'Chise Hatori', 'Izaya Orihara', 'Celty Sturluson', 'Rin Tousaka', 'Lawliet death note','Light Yagami', 'Lelouch', 'Mitsuha','Ciel Phantomhive', 'Yuna Gasai', 'Nana anime', 'Shiro', 'Hiyori noragami', 'Holo spice and wolf','Kaori Miyazono', 'Lisa Mishima', 'Rintaro Okabe', 'Levi Attack on Titan', 'Hachmian Hikigaya']
+
+#characters = ['Rintaro Okabe']
+
+def find_anime_character_url(character_name):
+	url = 'https://www.google.com/search?q=' + urllib.quote(character_name)
+	print url
+	r = requests.get(url)
+	data = r.text
+	soup = BeautifulSoup(data,  "html.parser")
+	results = soup.find_all("a", href=True)
+	#print results
+	for link in results:
+		if 'wikia' in link['href']: #/url?q=
+			return link['href'][7:].split('&', 1)[0]
+	
+	return ''
+
+def clean_data_string(data):
+	regex = re.compile(".*?\((.*?)\)")
+	return re.sub(r'\[.*\]', '', data).strip('\n').rstrip().lstrip()
+
+def scrape_weight(url):
+	return_data = {}
+	return_data['url'] = url
+	r = requests.get(url)
+	data = r.text
+	soup = BeautifulSoup(data,  "html.parser")
+	name = soup.find("h2", {"class": "pi-title"})
+	if name is not None:
+		return_data["name"] = name.text
+	else:
+		return_data["name"] = url.rsplit('/', 1)[-1]
+
+	association = soup.find("div", {"class":"wds-community-header__sitename"})
+	title = association.find("a").text
+	return_data["title"] = title
+	labels = soup.find_all("h3", {"class": "pi-data-label"})
+	values = soup.find("div", {"class" : "pi-data-value"})
+	relevant_values = ["weight", "height", "gender", "age"]
+	for i, label in enumerate(labels):
+		if label.text.lower() in relevant_values:
+			return_data[label.text.lower()] = clean_data_string(label.findNext('div').text)
+
+
+	if "weight" not in return_data or "height" not in return_data:
+		td_labels = soup.find_all("td")
+		for td in td_labels:
+			label = clean_data_string(td.get_text().lower())
+			if label in relevant_values:
+				return_data[label] = clean_data_string(td.findNext('td').text)
+
+
+	return return_data
+
+def write_to_file(data, filename):
+	with open(filename + '.txt', 'w') as outfile:
+		json.dump(data, outfile)
+
+
+if __name__ == '__main__':
+	data = []
+	for character in characters:
+		url = find_anime_character_url(character)
+		print url
+		if url:
+			data.append(scrape_weight(url))
+	write_to_file(data, "anime_character_stats")
+
