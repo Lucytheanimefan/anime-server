6dbbf89438c5b7dd0160b40f7b04380c06a10051
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-03-01 22:43:18 -0500

    Compute status

--- character_biostats/dataAnalysis.py
+++ character_biostats/dataAnalysis.py
@@ -8,13 +8,22 @@ def writeToCSV(filename):
 		new_data = []
 		info = ast.literal_eval(file.read())
 		with open("../data/data_new.csv", "w+") as write_file:
-			write_file.write("name,gender,height,weight,bmi\n")
+			write_file.write("name,gender,height,weight,bmi,status\n")
 			for data in info:
 				if "weight" in data and "height" in data:
-					height = float(data["height"])
-					weight = float(data["weight"])
-					bmi = weight/(height * height * 0.0001)
-					write_file.write(data["name"]+","+data["gender"]+","+str(height)+","+str(weight)+","+str(bmi)+"\n")
+					if len(data["height"])>0 and len(data["weight"])>0:
+						height = float(data["height"])
+						weight = float(data["weight"])
+						bmi = weight/(height * height * 0.0001)
+						status = 0
+						#-1=dead, 1=alive, 0=undefined
+						if "status" in data:
+							if "alive" in data["status"].lower():
+								status = 1
+							elif "deceased" in data["status"].lower():
+								status = -1
+						write_file.write(data["name"] + "," + data["gender"] + "," + str(height) + "," 
+							+ str(weight)+","+str(bmi) + "," + str(status) + "\n")
 			write_file.close()
 
 
@@ -23,13 +32,15 @@ def analyze(filename='../data/data.csv'):
 	groupby_gender = data.groupby('gender')
 	print(groupby_gender.mean())
 
+	print("---------------------------------")
 	# ordinary least squares
-	model = ols("height ~ weight", data).fit()
+	#model = ols("height ~ weight", data).fit()
+	model = ols("bmi ~ status", data).fit()
 	print(model.summary())
 	#for gender, value in groupby_gender['bmi']:
 		#print((gender,value))
 
 
 if __name__ == '__main__':
-	#writeToCSV("../data/new_cleaned_data.json")
+	writeToCSV("../data/new_cleaned_data.json")
 	analyze('../data/data_new.csv')
