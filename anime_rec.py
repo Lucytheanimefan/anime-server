734dc9780949b0003dcb850e9ac6b473e513ffb9
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-03-06 21:46:32 -0500

    encoding fix

--- anime_rec.py
+++ anime_rec.py
@@ -233,8 +233,8 @@ def findSeasonRecs(username, season, year, genre_count = None, studio_count = No
 	for anime in animez:
 		titlez = anime.find_all("h3",{"class":"main-title"})[0]
 		#print(titlez)
-		title = (titlez.text).strip().replace('"', "'").encode('utf-8')
-		print(title)
+		title = (titlez.text).strip().replace('"', "'")#.encode('utf-8')
+		#print(title)
 		print_data = (title == "Cutie Honey Universe")
 
 		scores[title] = 0 #each show starts off with 0
